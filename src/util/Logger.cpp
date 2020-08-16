0eb267738424d59a359b1d2c0d130fab9023ce26
Florian Hisch <florian.hisch@web.de>
2020-08-16 12:15:27 +0200

    Initial commit of new logger class.

---
+++ src/util/Logger.cpp
@@ -0,0 +1,63 @@
+#include "Logger.h"
+#include "../clock/clock.h"
+
+Logger singleton;
+
+Logger* Logger::Get() {
+    return &singleton;
+}
+
+void Logger::Log(const LoggerInterface* intf, const char* msg, LogLevel level) {
+    if(TestLogLevel(level)){
+        LogHeader(intf, level);
+        Log(msg);
+    }
+}
+
+void Logger::Append(const char* msg, LogLevel level) {
+    if(TestLogLevel(level)){
+        Log(msg);
+    }
+}
+
+void Logger::NewlineAppend(const char* msg, LogLevel level) {
+    if(TestLogLevel(level)){
+        Log("\n                                              "); // Skip what would be taken by the header instead
+        Log(msg);
+    }
+}
+
+void Logger::LogHeader(const LoggerInterface* intf, LogLevel level) {
+    // Print time
+    char str[32];
+    if(clock_was_init()) {
+        // Get current time
+        DateTime time = clock_now();
+
+        // Format time string and write it
+        sprintf(str, "\n%04hu/%02hu/%02hu %02hhu:%02hhu:%02hhu,%03hd ", time.year(), time.month(), time.day(), time.hour(), time.minute(), time.second(), (int) time.millis());
+    } else {
+        sprintf(str, "\n%23u ", (unsigned int) millis());
+    }
+    Log(str);
+
+    // Print log level
+    Log(LogLevelToString(level));
+
+    // Print intf
+    Log(intf->name);
+}
+
+void Logger::Log(const char* msg) {
+    Serial.write(msg);
+    int32_t n = snprintf(&log[logSize], LogMaxSize-logSize, msg);
+    if(n > 0)
+        logSize += n;
+}
+
+LoggerInterface::LoggerInterface(const char* name) {
+    uint16 len = strlen(name);
+    if(len > 12)
+        len = 12;
+    snprintf(this->name, 16, " [%s]%*c ", name, 11-len, ' ');
+}
---
+++ src/util/Logger.h
@@ -0,0 +1,136 @@
+#pragma once
+
+#include "Std_Types.h"
+
+class LoggerInterface; // Prototype declaration
+
+class Logger {
+
+public:
+    enum LogLevel {
+        LL_FATAL = 0,
+        LL_ERROR = 1,
+        LL_WARNING = 2,
+        LL_INFO = 3,
+        LL_DEBUG = 4
+    };
+
+    static const uint32 LogMaxSize = 65536;
+    static const LogLevel DefaultLogLevel = LL_INFO;
+
+public:
+    Logger(LogLevel logLevel = DefaultLogLevel) : logLevel(logLevel), logSize(0) { }
+
+    ~Logger() { }
+
+    static Logger* Get();
+
+    void Log(const LoggerInterface* intf, const char* msg, LogLevel level = LL_INFO);
+    void Append(const char* msg, LogLevel level = LL_INFO);
+    void NewlineAppend(const char* msg, LogLevel level = LL_INFO);
+
+    void SetLogLevel(LogLevel logLevel) {
+        this->logLevel = logLevel;
+    }
+
+    LogLevel GetLogLevel() const {
+        return logLevel;
+    }
+
+    bool TestLogLevel(LogLevel localLogLevel) const {
+        return (localLogLevel <= logLevel);
+    }
+
+private:
+    const char* LogLevelToString(LogLevel logLevel) const {
+        switch(logLevel) {
+        case LL_FATAL:
+            return "[FATAL] ";
+            break;
+        case LL_ERROR:
+            return "[ERROR] ";
+            break;
+        case LL_WARNING:
+            return "[WARN]  ";
+            break;
+        case LL_INFO:
+            return "[INFO]  ";
+            break;
+        case LL_DEBUG:
+            return "[DEBUG] ";
+            break;
+        default:
+            return "[UNKWN] ";
+        }
+    }
+
+    void LogHeader(const LoggerInterface* intf, LogLevel level);
+    void Log(const char* msg);
+
+private:
+    LogLevel logLevel;
+    uint32 logSize;
+    char log[LogMaxSize];
+};
+
+
+class LoggerInterface {
+
+public:
+    LoggerInterface(const char* name);
+
+    virtual void Log(const char* msg, Logger::LogLevel logLevel = Logger::LL_INFO) {
+        Logger::Get()->Log(this, msg, logLevel);
+    }
+
+    virtual void Append(const char* msg, Logger::LogLevel logLevel = Logger::LL_INFO) {
+        Logger::Get()->Append(msg, logLevel);
+    }
+
+    void Log(const char* msg, const char* str, Logger::LogLevel logLevel = Logger::LL_INFO) {
+        Logger::Get()->Log(this, msg, logLevel);
+        Logger::Get()->Append(str, logLevel);
+    }
+
+    void Log(const char* msg, uint32 i, Logger::LogLevel logLevel = Logger::LL_INFO) {
+        Logger::Get()->Log(this, msg, logLevel);
+        char str[16];
+        snprintf(str, 16, "%lu", i);
+        Logger::Get()->Append(str, logLevel);
+    }
+
+    void Log(const char* msg, int32 i, Logger::LogLevel logLevel = Logger::LL_INFO) {
+        Logger::Get()->Log(this, msg, logLevel);
+        char str[16];
+        snprintf(str, 16, "%ld", i);
+        Logger::Get()->Append(str, logLevel);
+    }
+
+    void Log(const char* msg, int32 i, const char* str, Logger::LogLevel logLevel = Logger::LL_INFO) {
+        Logger::Get()->Log(this, msg, logLevel);
+        char str2[16];
+        snprintf(str2, 16, "%ld", i);
+        Logger::Get()->Append(str2, logLevel);
+        Logger::Get()->Append(str, logLevel);
+    }
+
+    void Log(const char* msg, uint32 i, const char* str, Logger::LogLevel logLevel = Logger::LL_INFO) {
+        Logger::Get()->Log(this, msg, logLevel);
+        char str2[16];
+        snprintf(str2, 16, "%ld", i);
+        Logger::Get()->Append(str2, logLevel);
+        Logger::Get()->Append(str, logLevel);
+    }
+
+    void LogHexDump(const char* msg, const uint8* dump, uint32 len, Logger::LogLevel logLevel = Logger::LL_INFO) {
+        Logger::Get()->Log(this, msg, logLevel);
+        char str[16];
+        for(uint32 i = 0; i < len; i++) {
+            snprintf(str, 16, "%0hhX ", dump[i]);
+            Logger::Get()->Append(str, logLevel);
+        }
+    }
+
+public:
+    char name[16];
+};
